9f532a39f580371484fa0a2d1719b407d0c6c367
vaishnavi2833 <vaishnavi.ladda@wachandsangli.ac.in>
2023-08-28 21:43:58 +0530

    'fare-updated'

--- Backend/controllers/payment.js
+++ Backend/controllers/payment.js
@@ -10,7 +10,7 @@ const instance = new Razorpay({
 const checkOut = async (req, res) => {
   try{
     var options = {
-      amount: 50000,  // amount in the smallest currency unit
+      amount: 7000,  // amount in the smallest currency unit
       currency: "INR"
     };
     const order = await instance.orders.create(options)
